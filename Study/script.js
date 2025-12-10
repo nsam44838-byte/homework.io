87443ca1693c8ecefd7a3f2afa4b31c38cf2cf03
nsam44838-byte <nsam44838@gmail.com>
2025-12-10 09:38:49 +0700

    Add files via upload

---
+++ Study/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    menuToggle.classList.toggle('active');
+    // You would add logic here to show/hide your actual navigation links
+}
